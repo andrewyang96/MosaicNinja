6d5bca7a8e8cc67f6395336d0bf4975a94d6c678
Andrew Yang <ayang14@illinois.edu>
2015-09-12 01:59:15 -0400

    Download pictures

--- routes/mosaic.js
+++ routes/mosaic.js
@@ -6,26 +6,37 @@ var request = require('request');
 var Facebook = require('facebook-node-sdk');
 var facebook = new Facebook({ appID: config.fbAppID, secret: config.fbAppSecret });
 
+var kdt = require('kdt');
+var base64 = require('node-base64-image');
+var async = require('async');
+var _ = require('underscore');
+
 /* POST to trigger mosaic rendering */
 router.post('/mosaic', function (req, res, next) {
-  getLikes(req.body.fbid);
-  res.send('Mosaic making with theme ' + req.body.theme + ' started for ID ' + req.body.fbid);
+  getLikes(req.body.fbid, function (err, likes) {
+    console.log(likes);
+    res.send('Mosaic making with theme ' + req.body.theme + ' started for ID ' + req.body.fbid);
+  });
 });
 
 /* Begin mosaic making methods */
 
-var getLikes = function (id) {
+var getLikes = function (id, callback) {
   facebook.api('/' + id + '/likes?limit=100', function (err, data) {
     if (err) throw err;
     extractLikes(data, function (err, likes) {
       if (err) throw err;
-      console.log(likes);
+      callback(null, likes);
     });
   });
 };
 
 var extractLikes = function (likes, callback) {
-  var ret = likes.data || [];
+  var likesObj = likes.data || [];
+  var ret = [];
+  for (var i = 0; i < likesObj.length; i++) {
+    ret.push(likesObj[i].id);
+  }
   if (likes.paging.cursors.after) { // recursive case
     facebook.api('/' + id + '/likes?limit=100&after=' + likes.paging.cursors.after, function (err, data) {
       if (err) callback(err, null);
@@ -37,4 +48,24 @@ var extractLikes = function (likes, callback) {
   } else { // base case
     callback(null, ret);
   }
+};
+
+var downloadPictures = function (likes, callback) {
+  // likes - array of ids
+  // returns map of id keys and base 64 object values
+  async.map(likes, function (like) {
+    facebook.api('/' + like.id + '/picture?width=50&height=50', function (err, likeObj) {
+      if (err) throw err;
+      // encode image to base64
+      base64.base64encode(likeObj.data.url, { string: true }, function (err, saved) {
+        if (err) throw err;
+        return saved;
+      });
+    });
+  }, function (err, results) {
+    if (err) callback(err, null);
+    // zip the likes and results arrays
+    var retObj = _.object(likes, results);
+    callback(null, results);
+  });
 };
