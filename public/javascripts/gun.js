48f843d5f1b77fdbbace1fa36b1e1565faa14b1b
Andrew Yang <ayang14@illinois.edu>
2015-09-12 19:46:28 -0400

    Clean up code

--- public/javascripts/gun.js
+++
@@ -1,8 +0,0 @@
-var Gun = require('gun');
-var gun = Gun('https://gunjs.herokuapp.com/gun');
-gun.put({hello: "world"}).key('random/kUIkBxGFI');
-
-var ref = gun.get('random/kUIkBxGFI');
-ref.on(function(data){
-  $('body').text(JSON.stringify(data));
-});
