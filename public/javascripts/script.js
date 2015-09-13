e96420e3ed64e7a5631367d115859dd28da4bdcc
yhung119 <yhung119@illinois.edu>
2015-09-13 03:33:11 -0400

    change

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,26 +3,48 @@
 
 $('#mosaicform').submit(function(event){
 	event.preventDefault();
-	$.ajax({
+	var selected = $("select#choice").val();
+	var destinations = [];
+	var checkbox = $('.checkbox input:checked').each(function(){
+		destinations.push(this.value);
+
+		
+	});
+	for(var i =0; i<destinations.length; i++){
+		console.log(destinations[i]);
+	}
+	if((destinations.length>5 || destinations.length<2 )&& selected === "travel"){
+		console.log('error');
+		$('.error').replaceWith("<p>You need to select at least two and less than five</p>");
+	}else{
+		
+		$('.checkbox input:checked').removeAttr('checked');
+		$.ajax({
 		url: '/mosaic',
 		type: 'POST',
 		dataType: 'json',
-		data: {fbid: authData.userID, theme: 'like'},
+		data: {fbid: authData.userID, theme: selected, cities:destinations},
 		success: function(data){
 			console.log(data);
-			$('.animation').show()
+			$('.success').show();
 			$('#about').hide();
 			$(".progress").show();
 			$('.message').show();
 			$("#mosaicgenerator").hide();
 			$(".drop").hide();
-		}
+			}
 
-	})
+		})
+	}
+	
+	
+
+	
 });
 
 
 $(document).ready(function(){
+	$('.success').hide();
 	$("select#choice").val("like");
 	$('.checkbox').hide();
 	$('.animation').hide();
@@ -30,7 +52,7 @@ $(document).ready(function(){
   	$('.progress').hide();
   	$('.message').hide();
   	$('.drop').hide();
-	console.log($("select#choice").val());
+	
 	$("#like").click(function(){
 		$(".checkbox").hide();
 	});
@@ -47,6 +69,7 @@ $(document).ready(function(){
       $(".message").text($(this).text()+" is being generated!");
    });
 	$("#mosaicgenerator").click(function(){
+
 		
 	});
   
